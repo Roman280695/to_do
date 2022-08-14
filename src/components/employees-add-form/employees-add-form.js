65a55f8e5933b14705d6b2cdf665ce99baedd3d3
Roman <roman_petrov28@mail.ru>
2022-08-14 19:52:35 +0300

    marvel1

---
+++ src/components/employees-add-form/employees-add-form.js
@@ -0,0 +1,63 @@
+import {Component} from 'react';
+// import './employees-add-form.css';
+
+import './employees-add-form.scss';
+
+class EmployeesAddForm extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
+
+
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.name.length < 3 || !this.state.salary) return;
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
+
+    render () {
+        const {name, salary} = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex"
+                    onSubmit = {this.onSubmit}>
+                    <input type="text"
+                        className="form-control new-post-label"
+                        value={name}
+                        name="name"
+                        onChange={this.onValueChange}
+                        placeholder="Как его зовут?" />
+                    <input type="number"
+                        className="form-control new-post-label"
+                        value={salary}
+                        name="salary"
+                        onChange={this.onValueChange}
+                        placeholder="З/П в $?" />
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default EmployeesAddForm;
